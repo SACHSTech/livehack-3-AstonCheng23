1a94530943e7415ab910791a2033c7688f5a7ff3
Aston Cheng <aston.cheng23@ycdsbk12.ca>
2021-03-03 15:37:59 +0000

    added #comments

--- problem1.py
+++ problem1.py
@@ -12,6 +12,7 @@ loss_counter = 0
 final_results = " "
 games = 0 
 
+#This counts the wins and losses. Current correction code is non functioning.
 for games in range(6):
   results = input("Please enter in the results of your six games: \n")
   if results == "w":
@@ -22,9 +23,11 @@ for games in range(6):
     print("Please input either \"w\" or \"l\". ")
   final_results = str(results) + str(final_results)
 
+#This is just to keep an easier record of wins and losses.
 print(final_results)
 print("You won " + str(win_counter) + " times, and lost " + str(loss_counter) + " times.")
 
+#featuring a complicated system of win counters and loss counters to prevent errors. This checks the wins and losses and prints results.
 if win_counter == 6 or win_counter == 5 and loss_counter < 2: 
   print("Your team is in Group 1")
 if win_counter == 3 or win_counter == 4 and loss_counter < 3:
--- problem2.py
+++ problem2.py
@@ -10,17 +10,19 @@ mileage = 0
 average_distance = 0
 days = 0
 
+#This counts until the mileage passes 100. You can record your mileage here.
 while total_mileage < 100:
   print("___Day " + str(days) + "___")
   mileage = float(input("Enter in the distance driven for today in Kilometers: "))
   total_mileage = mileage + total_mileage
   days = days + 1
   
-
+#This is just to keep an easier record of wins and losses.
 print("\nSummary:")
 print("Total Mileage: " + str(total_mileage) + "km")
 print("Days: " + str(days) + "days\n")
 
+#This is to tell the user their average distance driven per day.
 if total_mileage >= 100:
   average_distance = total_mileage/days
   print("It took " + str(days) + " days to surpass 100km driven.")
